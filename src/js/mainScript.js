8b27db739ef8ca36a495a44b418e5b1e131b3e53
HEOSHII <george.shiryaev@gmail.com>
2022-07-13 17:20:01 +0300

    banner

--- src/js/mainScript.js
+++ src/js/mainScript.js
@@ -1,1 +1,29 @@
-const arr = [123123, 123123, 123, 123];
+const banner = document.querySelector('.banner');
+const bannerBackgorundItems = document.querySelectorAll('.banner-background-item');
+let x, y;
+window.addEventListener('mousemove', e => {
+  console.log(window.innerWidth);
+  if (e.clientX > window.innerWidth / 2) {
+    x = (e.clientX - window.innerWidth / 2) / 50;
+  } else {
+    x = -(window.innerWidth / 2 - e.clientX) / 50;
+  }
+  if (e.clientY > window.innerHeight / 2) {
+    y = (e.clientY - window.innerHeight / 2) / 50;
+  } else {
+    y = -(window.innerHeight / 2 - e.clientY) / 50;
+  }
+  moveItem(bannerBackgorundItems, x, y);
+});
+
+function moveItem(items, x, y) {
+  items.forEach(item => {
+    const distance = item.dataset.distance;
+    if (distance) {
+      console.log(distance);
+      item.style = `transform: translate(${x / distance}px,${y / distance}px)`;
+    } else {
+      item.style = `transform: translate(${x}px,${y}px)`;
+    }
+  });
+}
